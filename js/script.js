3662757b7ed1dfd6b0c0511fadfe7ae4621563b3
Selina Schöpfer <selina.schoepfer@stud.fhgr.ch>
2025-05-07 20:52:26 +0200

    just something

--- js/script.js
+++ js/script.js
@@ -13,12 +13,23 @@ const allWeatherData = await loadAllWeatherData();
 console.log(allWeatherData);
 
 
+// Unix-Timestamp in lesbare Zeit umwandeln
+function formatUnixToTime(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000);
+    return date.toLocaleTimeString('de-DE', {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+}
+console.log(formatUnixToTime(allWeatherData.current_weather.time));
+
+
 // Daten sortieren
-let sortedWeatherData = [];
+/* let sortedWeatherData = [];
 allWeatherData.forEach(location)
 
-console.log(sortedWeatherData);
+console.log(sortedWeatherData); */
 
 
 // Daten über DOM in HTML einfügen
-const startmap_container = document.querySelector('#locations');
+/* const startmap_container = document.querySelector('#locations'); */
